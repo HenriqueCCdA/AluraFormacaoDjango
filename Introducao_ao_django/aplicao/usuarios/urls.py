9e7eb786ce52882678aa4cc81d912caa7d96111a
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-10-27 01:29:03 -0300

    Fim da aula 4.

--- Introducao_ao_django/aplicao/usuarios/urls.py
+++ Introducao_ao_django/aplicao/usuarios/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import cadastro, login, dashboard, logout
+from .views import cadastro, login, dashboard, logout, cria_receita
 
 
 urlpatterns = [
     path('cadastro', cadastro, name='cadastro'),
     path('login', login, name='login'),
     path('dashboard', dashboard, name='dashboard'),
-    path('logout', logout, name='logout')
+    path('logout', logout, name='logout'),
+    path('cria_receita', cria_receita, name='cria_receita')
 ]
