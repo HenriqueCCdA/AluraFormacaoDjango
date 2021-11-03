5b8cec587f3ee2c1a1879db4df93c3925ef7aaa3
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-11-03 01:39:01 -0300

    Aula 3.

---
+++ TDD/tdd_busca_animal/animais/tests/test_urls.py
@@ -0,0 +1,19 @@
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from animais.views import index
+
+
+class AnimaisURLSTestCase(TestCase):
+
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_rota_url_utiliza_view_index(self):
+        '''
+        Teste se a home da aplicação utiliza o index da view
+        '''
+        request = self.factory.get('/')
+        with self.assertTemplateUsed('index.html'):
+            reponse = index(request)
+            self.assertEqual(reponse.status_code, 200)
